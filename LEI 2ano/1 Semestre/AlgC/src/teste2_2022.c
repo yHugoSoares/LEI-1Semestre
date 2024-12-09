fc820df96bf41dc8644754f34b19c5c1ec859eb5
Hugo Soares <hugoferreirasoares@gmail.com>
2024-12-09 00:22:45 +0000

    feat: added some teste2

--- LEI 2ano/1 Semestre/AlgC/src/teste2_2022.c
+++ LEI 2ano/1 Semestre/AlgC/src/teste2_2022.c
@@ -4,24 +4,24 @@
 typedef int WEIGHT;
 
 #define NE -1
-#define MAX 10
+#define MAX 5
 
 typedef WEIGHT GraphM[MAX][MAX];
 
-GraphM graphM ={{  0, NE, NE, NE, NE, NE, NE, NE, NE, NE },
-                { NE,  0, NE, NE, NE, NE, NE, 11, NE, NE },
-                { NE, NE,  0,  5, NE, NE, NE, NE, NE, NE }, // Edge: 2 -> 3 (weight 5)
-                { NE, NE,  5,  0, NE, NE, 12, NE, NE, NE }, // Edge: 3 -> 6 (weight 7)
-                { NE, 62, NE, NE,  0, NE, NE, NE, NE,  4 },
-                { NE, NE, NE, NE, NE,  0, NE, NE, NE, NE },
-                { NE, NE, NE, 12, NE, NE,  0, NE, NE, NE },
-                { NE, NE, 32, NE, NE, NE, NE,  0, NE, NE },
-                { NE, NE, NE, NE, NE, NE, NE, NE,  0, NE },
-                { NE, NE, NE, NE, 20, NE, NE,  8, NE,  0 }};
+#define NE -1
+
+int graphM[MAX][MAX] = {
+    { NE,   4,   3,  10,  NE },
+    {  4,  NE,   2,  NE,  40 },
+    {  3,   2,  NE,  NE,  NE },
+    { 10,  NE,  NE,  NE,   2 }, 
+    { NE,  40,  NE,   2,  NE }};
+
 
 typedef struct edge {
     int dest;
     WEIGHT weight;
+    struct edge *next;
 } *Edge, *GraphL[MAX];
 
 void create_exemploL(GraphL out){
@@ -45,36 +45,33 @@ void create_exemploL(GraphL out){
     out[3] = malloc(sizeof(struct edge));
     out[3]->dest = NE;
     out[3]->weight = NE;
+}
 
-    // Para o vértice 4:
-    out[4] = malloc(sizeof(struct edge));
-    out[4]->dest = 3;   // 4 -> 3
-    out[4]->weight = 1;
+void printGraphL(GraphL graphL) {
+    for (int i = 0; i < MAX; i++)
+    {
+        Edge x = graphL[i];
+        while (x != NULL)
+        {
+            printf("%d :%d\n", i, x->dest);
+            x = x->next;
+        }
+    }
+}
 
-    // Para o vértice 5:
-    out[5] = malloc(sizeof(struct edge));
-    out[5]->dest = 4;   // 5 -> 4
-    out[5]->weight = 2;
-
-    // Para o vértice 6:
-    out[6] = malloc(sizeof(struct edge));
-    out[6]->dest = 5;   // 6 -> 5
-    out[6]->weight = 3;
-
-    // Para o vértice 7:
-    out[7] = malloc(sizeof(struct edge));
-    out[7]->dest = NE;
-    out[7]->weight = NE;
-
-    // Para o vértice 8:
-    out[8] = malloc(sizeof(struct edge));
-    out[8]->dest = NE;
-    out[8]->weight = NE;
-
-    // Para o vértice 9:
-    out[9] = malloc(sizeof(struct edge));
-    out[9]->dest = 8;
-    out[9]->weight = 4;
+void fromMat(GraphM in, GraphL out) {
+    for (int i = 0; i < MAX; i++) {
+        out[i] = NULL;
+        for (int j = 0; j < MAX; j++) {
+            if (in[i][j] != NE) {
+                Edge newAresta = malloc(sizeof (struct edge));
+                newAresta->dest = j;
+                newAresta->weight = in[i][j];
+                newAresta->next = out[i];
+                out[i] = newAresta;
+            }
+        }
+    }
 }
 
 int pesoC (GraphM g, int V[], int k) {
@@ -83,42 +80,47 @@ int pesoC (GraphM g, int V[], int k) {
     {
         int custo = g[V[i]][V[i+1]];
         if (custo == NE) return -1;
-        result += custo;
+        else result += custo;
     }
     return result;
 }
 
-void componentes(GraphL g, int n, int comp[]) {
-    int v[n], counter;
-    for (int i = 0; i < n; i++) v[i] = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-        int c = i;
-        if (v[i] == 0)
-        {
-            Edge temp = g[i];
-            v[i] = 1;
-            comp[i] = counter;
-            while (temp != NULL && temp->dest != NE)
-            {
-                v[c] = 1;
-                c = temp->dest;
-                temp = g[c];
-            }
-            counter++;
-        }
+void dfs(GraphL g, int o, int comp[], int c) // travessia em profundidade; poderia ser em largura
+{
+    Edge p;
+    comp[o] = c;
+    for (p = g[o]; p; p = p->next) {
+        if (comp[p->dest] == -1) dfs(g, p->dest, comp, c);
     }
-} // Not working
+}
+
+void componentes(GraphL g, int n, int comp[])
+{
+    int i, c = 0;
+    // c = índice dos componentes, começando em 0
+    for (i = 0; i < n; i++) comp[i] = -1;
+    // este array servirá também para controlar a travessia
+    // dispensando um array de cores ou 'visitados'
+    for (i = 0; i < n; i++)
+        if (comp[i] == -1) {
+            dfs(g, i, comp, c);
+            c++;
+        }
+}
 
 int main () {
     int v[] = {2,3,6}, comp[MAX];
     GraphL graphL;
-    create_exemploL(graphL);
+    fromMat(graphM, graphL);
+
+    
+    
+    
     printf("PesoC: %d\n", pesoC(graphM, v, 3));
 
     
-    printf("Componentes:\n");
+    printf("Componentes: ");
     componentes(graphL, MAX, comp);
     int length = sizeof(comp) / sizeof(comp[0]);
     for (int i = 0; i < MAX; i++)
